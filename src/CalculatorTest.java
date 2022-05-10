9b58604bb35fd8ea82664146c5a2f3879ab6153f
Salty Tuna <82872340+Salty-tuna-gitbash@users.noreply.github.com>
2022-05-10 22:42:57 +0800

    update-calc-update

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,7 +1,58 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.testng.AssertJUnit.assertEquals;
 public class CalculatorTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    @DisplayName("Exponent")
+    void shouldShowtheResultFactorial(int value){
+        Calculator calc = new Calculator();
+        assertEquals(value, calc.sum(value, 1));
+    }
 
+    //factorial
+    @Test
+    void testofFactorial() {
+        Calculator calc = new Calculator();
+        // 2! = 2
+        assertEquals(6, calc.sum(1, 3));
+    }
 
-        void diplaySum () {
-            System.out.println("test");
+    @ParameterizedTest
+    @ValueSource(ints = {3,4,5,6,7})
+    @DisplayName("Factorial")
+    void ShowtheResultFactorial(int value){
+        Calculator calc = new Calculator();
+        if(value == 3){
+            assertEquals(18, calc.sum(value,3));
+        }else if(value == 4){
+            assertEquals(96, calc.sum(value,4));
+        }else if(value == 5){
+            assertEquals(600, calc.sum(value,5));
+        }else if(value == 6){
+            assertEquals(4320, calc.sum(value,6));
+        }else if(value == 7){
+            assertEquals(35280, calc.sum(value,7));
         }
+    }
+
+    //palindrome
+    @ParameterizedTest
+    @ValueSource(strings = {"wow","refer", "civic", "racecar", "kayak" })
+    @DisplayName("Palindrome")
+    void testThePalindrome(String value){
+        Calculator calc = new Calculator();
+        assertEquals(value, calc.Palindrome(value));
+    }
+
 }
